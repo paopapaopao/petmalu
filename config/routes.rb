23041a0cf395915db927f20aadbcc6234b7edb26
paopapaopao <paolo.casugay@gmail.com>
2021-09-13 21:47:45 +0800

    Install rails_admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 end
