d182e63765e3315f8b836de1f7bec27b0dcd48bf
paopapaopao <paolo.casugay@gmail.com>
2021-09-16 20:47:25 +0800

    Add first name to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable
+
+  validates :first_name, presence: true
 end
