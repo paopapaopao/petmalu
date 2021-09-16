f7a7a279148610393366c1e9a6621e9a45c1f440
paopapaopao <paolo.casugay@gmail.com>
2021-09-16 15:26:01 +0800

    Add devise user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  devise_for :users
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 end
