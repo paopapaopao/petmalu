1ed82edc4c74f2014ecdf871af8bff477adc1148
paopapaopao <paolo.casugay@gmail.com>
2021-09-17 18:31:26 +0800

    Add users factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :user do
+    email { Faker::Internet.unique.email }
+    password { Faker::Internet.password(min_length: 6, max_length: 128) }
 
+    confirmed_at { DateTime.now }
+
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    admin { false }
+    approved { false }
   end
 end
