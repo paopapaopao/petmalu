0248b8f0acd0dbb758aa0d21e26337bf58d77f8a
paopapaopao <paolo.casugay@gmail.com>
2021-09-19 11:56:45 +0800

    Add comments controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 end
