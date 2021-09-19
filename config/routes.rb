1071b4b110eace606c20f4139c7d843ab249793d
Ed Salinas <edsalinas7b@yahoo.com>
2021-09-19 12:27:56 +0800

    home views added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  
   devise_for :users
 
-  resources :posts
+  resources :posts, :home
+  
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 end
