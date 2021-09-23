9949587e9f8899f069c22b480d80bbf6408d4678
paopapaopao <paolo.casugay@gmail.com>
2021-09-23 19:54:36 +0800

    Remove resources routes for home

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+
+  root 'home#index'
+
   devise_for :users
 
-  resources :posts, :home
+  resources :posts do
+    resources :comments
+  end
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 end
