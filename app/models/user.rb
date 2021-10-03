3729663930b8614672b8edf36251014d9fd4a4d9
paopapaopao <paolo.casugay@gmail.com>
2021-10-03 10:54:09 +0800

    Add mailer methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,41 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
 
-  has_many :posts
-  has_many :comments
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
+  attr_accessor :approved_before_update
+
+  after_create :set_approved_before_update
+  after_update :admin_new_user_approval
+  after_update :user_approved
 
   def full_name
     "#{first_name.capitalize} #{last_name.capitalize}"
+  end
+
+  def active_for_authentication?
+    super && approved?
+  end
+
+  def inactive_message
+    approved? ? super : :not_approved
+  end
+
+  def set_approved_before_update
+    @approved_before_update = approved?
+  end
+
+  def admin_new_user_approval
+    if confirmed_at && !approved?
+      UserMailer.admin_new_user_approval(id).deliver_later
+    end
+  end
+
+  def user_approved
+    if confirmed_at && approved? && !@approved_before_update
+      set_approved_before_update
+      UserMailer.user_approved.deliver_later
+    end
   end
 end
