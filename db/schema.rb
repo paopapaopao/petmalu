86b718a25227e9fcb8004443eb3f5536e07ded19
paopapaopao <paolo.casugay@gmail.com>
2021-10-04 15:06:06 +0800

    Add photo attribute/column to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_27_130528) do
+ActiveRecord::Schema.define(version: 2021_10_04_061910) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,10 @@ ActiveRecord::Schema.define(version: 2021_09_27_130528) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.string "photo_file_name"
+    t.string "photo_content_type"
+    t.bigint "photo_file_size"
+    t.datetime "photo_updated_at"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
