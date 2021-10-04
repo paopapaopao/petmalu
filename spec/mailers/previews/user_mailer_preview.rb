c5603d0c1374e3f898809ac28a1171c318ff42e9
paopapaopao <paolo.casugay@gmail.com>
2021-10-04 14:00:06 +0800

    Add user mailer spec

---
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -0,0 +1,9 @@
+# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
+class UserMailerPreview < ActionMailer::Preview
+
+  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/user_approved
+  def user_approved
+    UserMailer.user_approved
+  end
+
+end
