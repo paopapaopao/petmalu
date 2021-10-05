f97aee6331b8aa9d20453d327f5d434e81aa144d
paopapaopao <paolo.casugay@gmail.com>
2021-10-05 19:23:00 +0800

    Add photo validation and association to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,10 @@ class Post < ApplicationRecord
 
   belongs_to :user
   has_many :comments, dependent: :destroy
+
+  has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+
+  validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
+
   acts_as_votable
 end
