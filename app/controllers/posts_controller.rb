9257a75425c4955e289a198e1e909106e93f1360
paopapaopao <paolo.casugay@gmail.com>
2021-10-09 16:45:03 +0800

    Run create.js after creating a post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -54,6 +54,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
+        format.js
         format.html { redirect_to @post, notice: "Post was successfully created." }
         format.json { render :show, status: :created, location: @post }
       else
