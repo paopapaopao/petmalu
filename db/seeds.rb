02c5b66b3002435859f2e7a39a407aed3c8661ab
paopapaopao <paolo.casugay@gmail.com>
2021-10-12 23:26:32 +0800

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,34 +33,32 @@ pong = User.create(
   approved: true
 )
 
-4.times do
-  Post.create(
-    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    user_id: juan.id
-  )
-  Comment.create(
-    body: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    user_id: juan.id,
-    post_id: Post.last.id
-  )
-  Comment.create(
-    body: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    user_id: pong.id,
-    post_id: Post.last.id
-  )
+Post.create(
+  content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  user_id: juan.id
+)
+Comment.create(
+  body: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  user_id: juan.id,
+  post_id: Post.last.id
+)
+Comment.create(
+  body: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  user_id: pong.id,
+  post_id: Post.last.id
+)
 
-  Post.create(
-    content: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
-    user_id: juan.id
-  )
-  Comment.create(
-    body: "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
-    user_id: juan.id,
-    post_id: Post.last.id
-  )
-  Comment.create(
-    body: "Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.",
-    user_id: pong.id,
-    post_id: Post.last.id
-  )
-end
+Post.create(
+  content: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
+  user_id: juan.id
+)
+Comment.create(
+  body: "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
+  user_id: juan.id,
+  post_id: Post.last.id
+)
+Comment.create(
+  body: "Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.",
+  user_id: pong.id,
+  post_id: Post.last.id
+)
