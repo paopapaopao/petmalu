235a421b5a760e8dc45ac1a7f6c5250c621a99e4
paopapaopao <paolo.casugay@gmail.com>
2021-10-14 18:39:57 +0800

    Assign a default photo

--- app/services/search_pet_service.rb
+++ app/services/search_pet_service.rb
@@ -1,11 +1,18 @@
 class SearchPetService < ApplicationService
   def initialize(params)
     @params = params
+    @params.compact!
+    @params.delete_if { |k, v| v == '' }
   end
 
   def call
     pets, pagination = Petfinder::Client.new.animals(params: @params)
 
-    pets
+    photos_full = pets.map do |pet|
+      pet.primary_photo_cropped &&= pet.primary_photo_cropped.full
+      pet.primary_photo_cropped || 'https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/65761296352685.5eac4787a4720.jpg'
+    end
+
+    [pets, photos_full]
   end
 end
