1146bc8f7200233548ac41d2d758f4073497ec42
paopapaopao <paolo.casugay@gmail.com>
2021-10-15 17:39:22 +0800

    Refactor

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,23 +1,29 @@
 class CommentsController < ApplicationController
+  before_action :set_post
+
   def create
-    @post = Post.find(params[:post_id])
     @comment = @post.comments.new(comment_params)
     @comment.user_id = current_user.id
     authorize @comment
     @comment.save
+
     redirect_to post_path(@post)
   end
 
   def destroy
-    @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
     authorize @comment
     @comment.destroy
+
     redirect_to post_path(@post)
   end
 
   private
     def comment_params
       params.require(:comment).permit(:body)
+    end
+
+    def set_post
+      @post = Post.find(params[:post_id])
     end
 end
