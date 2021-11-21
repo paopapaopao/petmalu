7393e47c401aa0b4e44a71ab9213d3a8f5879393
paopapaopao <paolo.casugay@gmail.com>
2021-11-21 14:46:45 +0800

    Remove photo from posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_05_062834) do
+ActiveRecord::Schema.define(version: 2021_10_16_120840) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,15 +25,25 @@ ActiveRecord::Schema.define(version: 2021_10_05_062834) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
+  create_table "mentions", id: :serial, force: :cascade do |t|
+    t.string "mentionee_type"
+    t.integer "mentionee_id"
+    t.string "mentioner_type"
+    t.integer "mentioner_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["mentionee_id", "mentionee_type", "mentioner_id", "mentioner_type"], name: "mentions_mentionee_mentioner_idx", unique: true
+    t.index ["mentionee_id", "mentionee_type"], name: "mentions_mentionee_idx"
+    t.index ["mentionee_type", "mentionee_id"], name: "index_mentions_on_mentionee"
+    t.index ["mentioner_id", "mentioner_type"], name: "mentions_mentioner_idx"
+    t.index ["mentioner_type", "mentioner_id"], name: "index_mentions_on_mentioner"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
-    t.string "photo_file_name"
-    t.string "photo_content_type"
-    t.bigint "photo_file_size"
-    t.datetime "photo_updated_at"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
